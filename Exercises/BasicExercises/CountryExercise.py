04a5e4f9b54b6dd19061ed6c7ace0986cabf0ea7
Ayaan <53174223+astro648@users.noreply.github.com>
2022-09-09 18:27:04 -0700

    fixed concatenation issues and added population meter

--- Exercises/BasicExercises/CountryExercise.py
+++ Exercises/BasicExercises/CountryExercise.py
@@ -1,6 +1,11 @@
 country = 'United States'
 continent = 'North America'
 population = 331450000
-print(country)
-print(continent)
-print(population)
+print("Country: "+country)
+print("Continent: "+continent)
+print("Population: "+str(population))
+
+if(population < 33000000):
+    print(country+"'s population is below average")
+else:
+    print(country+"'s name is below average")
--- Exercises/BasicExercises/StringConcatenation.py
+++ Exercises/BasicExercises/StringConcatenation.py
@@ -10,4 +10,4 @@ print("This message and it's meaning")
 print("     were formatted in Python")
 
 lenstr3: str = len(str3)
-print("Length of string \""+str3+"\" is "+lenstr3)
+print("Length of string \""+str3+"\" is "+str(lenstr3))
