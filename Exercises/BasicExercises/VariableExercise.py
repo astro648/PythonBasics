17346545654be57c32d90da4a272cbb28e0725ca
Ayaan <53174223+astro648@users.noreply.github.com>
2022-09-09 17:56:18 -0700

    variable exercise

---
+++ Exercises/BasicExercises/VariableExercise.py
@@ -0,0 +1,17 @@
+studentName = 'John Doe'
+discountedPrice = '2.99'
+juiceBottles = '256'
+milesTraveled = '92'
+highestTestScore = '100'
+
+x = 1
+y = 2
+z = x + y
+
+fName = 'John'
+name = 'John Doe'
+age = 27
+print('First name: '+fName)
+print('Full name: '+Name)
+print('Age: '+age)
+
