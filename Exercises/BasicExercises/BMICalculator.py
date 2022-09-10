6198be826959bdd929f9e5233f602684771d6251
Ayaan <53174223+astro648@users.noreply.github.com>
2022-09-09 18:09:47 -0700

    bmi calculator

---
+++ Exercises/BasicExercises/BMICalculator.py
@@ -0,0 +1,9 @@
+alenMass = 60
+alenHeight = 1.70
+ericMass = 95
+ericHeight = 1.88
+if alenMass / alenHeight * 2 > ericMass / ericHeight * 2:
+    print("Alen's BMI is higher than Eric's!")
+else:
+    print("Eric's BMI is higher than Alen's!")
+
