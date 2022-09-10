ba7bbc2284fda730e14315a10f502deed5749b2c
Ayaan <53174223+astro648@users.noreply.github.com>
2022-09-09 18:23:53 -0700

    slightly bugged string concatenation

---
+++ Exercises/BasicExercises/StringConcatenation.py
@@ -0,0 +1,13 @@
+str1 = "Python"
+str2 = "3.10"
+str3 = "Python is fun"
+
+print(str1 + " " + str2)
+print("")
+
+print("Hello my friend, ")
+print("This message and it's meaning")
+print("     were formatted in Python")
+
+lenstr3: str = len(str3)
+print("Length of string \""+str3+"\" is "+lenstr3)
