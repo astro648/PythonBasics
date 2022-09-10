6ce7eadb8bb9c58f98513f29113c61646308f312
Ayaan <53174223+astro648@users.noreply.github.com>
2022-09-09 18:47:56 -0700

    fixed grade not registering

--- Exercises/BasicExercises/EnterGrade.py
+++ Exercises/BasicExercises/EnterGrade.py
@@ -1,14 +1,14 @@
 print("Input grade:")
-grade = input()
+grade = int(input()) # this makes the input an integer
 if grade == 4:
     print("A")
 elif grade == 3:
-    print("A")
+    print("B")
 elif grade == 2:
-    print("A")
+    print("C")
 elif grade == 1:
-    print("A")
+    print("D")
 elif grade == 0:
-    print("A")
+    print("F")
 else:
     print("Grade invalid, type in 4, 3, 2, 1, or 0")
