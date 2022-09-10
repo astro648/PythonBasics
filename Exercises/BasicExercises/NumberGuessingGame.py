a75f2438a3e6c88f8e35bad421423e688643454f
Ayaan <53174223+astro648@users.noreply.github.com>
2022-09-09 19:10:35 -0700

    fix issue with number game

--- Exercises/BasicExercises/NumberGuessingGame.py
+++ Exercises/BasicExercises/NumberGuessingGame.py
@@ -1,5 +1,15 @@
-randnum = r.randint(0, 10)
-print("Random number generated from 0 to 10, input answer:")
-solution = input()
-while solution != randnum:
-    print("Wrong number")
+import random as r
+
+def randnumgame():
+    randnum = r.randint(0, 10)
+    print("--")
+    print("Random number generated from 0 to 10, input answer:")
+    solution = int(input())
+    if solution != randnum:
+        print("Wrong number")
+        randnumgame()
+    else:
+        print("Correct number")
+        randnumgame()
+
+randnumgame()
