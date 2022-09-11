6974544d5cbe038178c50d4cc89e95770bda818f
Ayaan <53174223+astro648@users.noreply.github.com>
2022-09-10 20:16:12 -0700

    rollback and change function name to pascalcase

--- Exercises/BasicExercises/NumberGuessingGame.py
+++ Exercises/BasicExercises/NumberGuessingGame.py
@@ -1,12 +1,12 @@
 import random as r
 
 
-def RandNumGame(myInput):
-    solution = r.randint(0, 10)
+def RandNumGame():
+    randnum = r.randint(0, 10)
     print("--")
     print("Random number generated from 0 to 10, input answer:")
-
-    if myInput != solution:
+    solution = int(input())
+    if solution != randnum:
         print("Wrong number")
         RandNumGame()
     else:
@@ -14,5 +14,4 @@ def RandNumGame(myInput):
         RandNumGame()
 
 
-myInput = str(input())  # making it a parameter
-RandNumGame(myInput)
+RandNumGame()
