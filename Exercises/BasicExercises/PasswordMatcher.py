d7e2bf1f79d64c3e5da4747b217dff8ebd01776a
Ayaan <53174223+astro648@users.noreply.github.com>
2022-09-16 19:07:03 -0700

    5 attempts

--- Exercises/BasicExercises/PasswordMatcher.py
+++ Exercises/BasicExercises/PasswordMatcher.py
@@ -1,13 +1,17 @@
 passwordTuple = ("123456", "123456789", "qwerty", "password", "admin")
 
-def passwordInput():
+def password():
+    attempts = 5
     x = 0
-    pwdinput = input("Password: ")
-    for i in passwordTuple:
-        if pwdinput == i:
-            print("Correct")
-            x = 1
-    if x == 0:
-        print("Try again")
-        passwordInput()
-passwordInput()
+    while attempts != 0 and x != 1:
+        pwdinput = input("Password: ")
+        for i in passwordTuple:
+            if pwdinput == i:
+                print("Correct")
+                x = 1
+        if x == 0:
+            print("Try again")
+            attempts = attempts - 1
+            print(str(attempts)+" attempts left")
+
+password()
