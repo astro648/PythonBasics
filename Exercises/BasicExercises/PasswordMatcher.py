017da8092efceb15543883cf05b819bf1d0ffad4
Ayaan <53174223+astro648@users.noreply.github.com>
2022-09-16 18:16:38 -0700

    Update PasswordMatcher.py

--- Exercises/BasicExercises/PasswordMatcher.py
+++ Exercises/BasicExercises/PasswordMatcher.py
@@ -1,2 +1,9 @@
 passwordTuple = ("123456", "123456789", "qwerty", "password", "admin")
-
+def passwordInput():
+    pwdinput = input("Password: ")
+    if pwdinput == passwordTuple(0) or pwdinput == passwordTuple(1) or pwdinput == passwordTuple(2) or pwdinput == passwordTuple(3) or pwdinput == passwordTuple(4):
+        print("Correct")
+        passwordInput()
+    else:
+        print("Incorrect, try again")
+        passwordInput()
