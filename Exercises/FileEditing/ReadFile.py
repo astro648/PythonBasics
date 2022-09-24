c155e63434c06d9514ceef86b31283d4ac2a1d6b
Ayaan <53174223+astro648@users.noreply.github.com>
2022-09-23 18:22:49 -0700

    Update ReadFile.py

--- Exercises/FileEditing/ReadFile.py
+++ Exercises/FileEditing/ReadFile.py
@@ -1,6 +1,10 @@
-f = open("lipsumpar.txt")
-# line = f.readline()
-for line in f:
-    print(line.strip())
-
-f.close()
+import os.path
+fileExist = os.path.exists("e.txt")
+if fileExist is True:
+    f = open("lipsumpar.txt")
+    # line = f.readline()
+    for line in f:
+        print(line.strip())
+    f.close()
+else:
+    print("File does not exist")
