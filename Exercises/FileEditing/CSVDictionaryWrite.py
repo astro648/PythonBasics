dcdb7f4a1abd42dee371f24b95501e63a24fea39
Ayaan <53174223+astro648@users.noreply.github.com>
2022-09-25 09:04:00 -0700

    formatting + import

--- Exercises/FileEditing/CSVDictionaryWrite.py
+++ Exercises/FileEditing/CSVDictionaryWrite.py
@@ -5,5 +5,8 @@ with open('1wklaunchwrite.csv', mode='w') as csv_file:
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
     writer.writeheader()
-    writer.writerow({'Spacecraft': 'NROL-91', 'Rocket': 'Delta IV Heavy', 'Company': 'United Launch Alliance', 'Date of Launch': '24-Sep-22'})
-    writer.writerow({'Spacecraft': 'Starlink Group 4-35', 'Rocket': 'Falcon 9 Block 5', 'Company': 'SpaceX', 'Date of Launch': '24-Sep-22'})
+    writer.writerow({'Spacecraft': 'NROL-91', 'Rocket': 'Delta IV Heavy', 'Company': 'United Launch Alliance',
+                     'Date of Launch': '24-Sep-22'})
+    writer.writerow({'Spacecraft': 'Starlink Group 4-35', 'Rocket': 'Falcon 9 Block 5', 'Company': 'SpaceX',
+                     'Date of Launch': '24-Sep-22'})
+    
