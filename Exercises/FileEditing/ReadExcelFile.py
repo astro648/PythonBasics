ba292ba1dde223657078e01b2c18d390ce52ee6a
Ayaan <53174223+astro648@users.noreply.github.com>
2022-09-25 09:17:50 -0700

    variable

--- Exercises/FileEditing/ReadExcelFile.py
+++ Exercises/FileEditing/ReadExcelFile.py
@@ -2,3 +2,9 @@
 import xlrd
 import openpyxl
 
+# define variable to load the dataframe
+dataframe = openpyxl.load_workbook('1MonthOfLaunches.xlsx')
+
+# define variable to read sheet
+dataframe1 = dataframe.active
+
