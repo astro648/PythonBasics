2d0190818c9153a689edfb35267678b67d612b44
Ayaan <53174223+astro648@users.noreply.github.com>
2022-09-25 09:25:55 -0700

    slightly bugged output

--- Exercises/FileEditing/ReadExcelFile.py
+++ Exercises/FileEditing/ReadExcelFile.py
@@ -8,3 +8,7 @@ dataframe = openpyxl.load_workbook('1MonthOfLaunches.xlsx')
 # define variable to read sheet
 dataframe1 = dataframe.active
 
+# read cell values
+for row in range(0, dataframe1.max_row):
+    for col in dataframe1.iter_cols(1, dataframe1.max_column):
+        print(col[row].value)
