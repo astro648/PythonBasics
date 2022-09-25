31646886f315efb352903b2d26d2fda4700a8538
Ayaan <53174223+astro648@users.noreply.github.com>
2022-09-24 18:59:14 -0700

    write in dictionary

---
+++ Exercises/FileEditing/CSVDictionaryWrite.py
@@ -0,0 +1,9 @@
+import csv
+
+with open('1wklaunch.csv', mode='w') as csv_file:
+    fieldnames = ['Spacecraft', 'Rocket', 'Company', 'Date of Launch']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+    writer.writeheader()
+    writer.writerow({'Spacecraft': 'NROL-91', 'Rocket': 'Delta IV Heavy', 'Company': 'United Launch Alliance', 'Date of Launch': '24-Sep-22'})
+    writer.writerow({'Spacecraft': 'Starlink Group 4-35', 'Rocket': 'Falcon 9 Block 5', 'Company': 'SpaceX', 'Date of Launch': '24-Sep-22'})
--- Exercises/FileEditing/CSVEditing.py
+++ Exercises/FileEditing/CSVEditing.py
@@ -1,2 +1,14 @@
 import csv
 
+with open('1wklaunch.csv') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter = ',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {",".join(row)}')
+            print("")
+            line_count += 1
+        else:
+            print(f'\t{row[0]} is getting launched on the {row[1]}')
+            print("")
+            line_count += 1
